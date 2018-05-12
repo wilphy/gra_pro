98d7fa369f14db86128d291f366f48acbc4a9a21
wilphy <149201216@qq.com>
2018-05-12 11:58:28 +0800

    add route

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const mysql = require('mysql');
 const url = require('url');
 const express = require('express');
 const path = require('path');
+const router = require('./routes/route')
 
 
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.static('static'));
+//静态文件读取
+app.use('/public', express.static('public'));
+app.use('/static', express.static('static'));
+app.use(express.static('views'));
 
 //数据库
 let db = mysql.createPool({
@@ -19,6 +22,7 @@ let db = mysql.createPool({
   database: 'gra'
 });
 
+//测试数据库连接
 db.query(`SELECT * FROM stu_user`, (err, data) => {
   if (err) {
     console.log(err);
@@ -27,182 +31,184 @@ db.query(`SELECT * FROM stu_user`, (err, data) => {
   }
 });
 
+app.use(router);
+
+app.listen(8080);
+
 //hhtp服务器 登录、注册
-let httpServer = http.createServer((req, res) => {
+// let httpServer = http.createServer((req, res) => {
 
-  let {
-    pathname,
-    query
-  } = url.parse(req.url, true);
+//   let {
+//     pathname,
+//     query
+//   } = url.parse(req.url, true);
 
-  //注册接口
-  if (pathname == '/reg') {
+//   //注册接口
+//   if (pathname == '/reg') {
 
-    let {
-      user,
-      pass
-    } = query;
+//     let {
+//       user,
+//       pass
+//     } = query;
 
-    //1.检验数据
-    if (!/^\w{6,32}$/.test(user)) {
-      res.write(JSON.stringify({
-        code: 1,
-        msg: '用户名不符合规范'
-      }));
-      res.end();
+//     //1.检验数据
+//     if (!/^\w{6,32}$/.test(user)) {
+//       res.write(JSON.stringify({
+//         code: 1,
+//         msg: '用户名不符合规范'
+//       }));
+//       res.end();
 
-    } else if (!/^.{6,32}$/.test(pass)) {
-      res.write(JSON.stringify({
-        code: 1,
-        msg: '密码不符合规范'
-      }));
-      res.end();
+//     } else if (!/^.{6,32}$/.test(pass)) {
+//       res.write(JSON.stringify({
+//         code: 1,
+//         msg: '密码不符合规范'
+//       }));
+//       res.end();
 
-    } else {
+//     } else {
 
-      //2.检验用户名是否重复
-      db.query(`SELECT * FROM stu_user WHERE username='${user}'`, (err, data) => {
-        if (err) {
-          res.write(JSON.stringify({
-            code: 1,
-            msg: '数据库有误'
-          }));
-          res.end();
+//       //2.检验用户名是否重复
+//       db.query(`SELECT * FROM stu_user WHERE username='${user}'`, (err, data) => {
+//         if (err) {
+//           res.write(JSON.stringify({
+//             code: 1,
+//             msg: '数据库有误'
+//           }));
+//           res.end();
 
-        } else if (data.length > 0) {
-          res.write(JSON.stringify({
-            code: 1,
-            msg: '用户名已存在'
-          }));
-          res.end();
+//         } else if (data.length > 0) {
+//           res.write(JSON.stringify({
+//             code: 1,
+//             msg: '用户名已存在'
+//           }));
+//           res.end();
 
-        } else {
+//         } else {
 
-          //3.插入
-          db.query(`INSERT INTO stu_user (username, password,online) VALUES('${user}', '${pass}', 0)`, err => {
-            if (err) {
-              res.write(JSON.stringify({
-                code: 1,
-                msg: '数据库有误'
-              }));
-              res.end();
+//           //3.插入
+//           db.query(`INSERT INTO stu_user (username, password,online) VALUES('${user}', '${pass}', 0)`, err => {
+//             if (err) {
+//               res.write(JSON.stringify({
+//                 code: 1,
+//                 msg: '数据库有误'
+//               }));
+//               res.end();
 
-            } else {
-              res.write(JSON.stringify({
-                code: 0,
-                msg: '注册成功'
-              }));
+//             } else {
+//               res.write(JSON.stringify({
+//                 code: 0,
+//                 msg: '注册成功'
+//               }));
 
-              res.end();
+//               res.end();
 
-              // fs.readFile(`/views/reg_succes.html`, (err, data) => {
-              //   if(err) {
-              //     res.writeHead(404);
-              //     res.write('Not Found');
-              //   } else {
-              //     res.write(data);
-              //   }
-              // });
+//               // fs.readFile(`/views/reg_succes.html`, (err, data) => {
+//               //   if(err) {
+//               //     res.writeHead(404);
+//               //     res.write('Not Found');
+//               //   } else {
+//               //     res.write(data);
+//               //   }
+//               // });
 
-              // res.end();
+//               // res.end();
 
-            }
-          });
-        }
-      });
-    }
+//             }
+//           });
+//         }
+//       });
+//     }
 
-    console.log('请求了注册', query);
-
-
-    //登录接口
-  } else if (pathname == '/login') {
+//     console.log('请求了注册', query);
 
 
-    let {
-      user,
-      pass
-    } = query;
+//     //登录接口
+//   } else if (pathname == '/login') {
 
-    //1.校验数据
-    if (!/^\w{6,32}$/.test(user)) {
-      res.write(JSON.stringify({
-        code: 1,
-        msg: '用户名不符合规范'
-      }));
-      res.end();
 
-    } else if (!/^\w{6,32}$/.test(pass)) {
-      res.write(JSON.stringify({
-        code: 1,
-        msg: '密码不符合规范'
-      }));
-      res.end();
+//     let {
+//       user,
+//       pass
+//     } = query;
 
-    } else {
-      //2.取数据
-      db.query(`SELECT ID,password FROM stu_user WHERE username='${user}'`, (err, data) => {
-        if (err) {
-          res.write(JSON.stringify({
-            code: 1,
-            msg: '数据库有误'
-          }));
-          res.end();
+//     //1.校验数据
+//     if (!/^\w{6,32}$/.test(user)) {
+//       res.write(JSON.stringify({
+//         code: 1,
+//         msg: '用户名不符合规范'
+//       }));
+//       res.end();
 
-        } else if (data.length == 0) {
-          res.write(JSON.stringify({
-            code: 1,
-            msg: '用户名不存在'
-          }));
-          res.end();
+//     } else if (!/^\w{6,32}$/.test(pass)) {
+//       res.write(JSON.stringify({
+//         code: 1,
+//         msg: '密码不符合规范'
+//       }));
+//       res.end();
 
-        } else if (data[0].password != pass) {
-          res.write(JSON.stringify({
-            code: 1,
-            msg: '用户名或密码错误'
-          }));
-          res.end();
+//     } else {
+//       //2.取数据
+//       db.query(`SELECT ID,password FROM stu_user WHERE username='${user}'`, (err, data) => {
+//         if (err) {
+//           res.write(JSON.stringify({
+//             code: 1,
+//             msg: '数据库有误'
+//           }));
+//           res.end();
 
-        } else {
-          //3.设置状态
-          db.query(`UPDATE stu_user SET online=1 WHERE ID=${data[0].ID}`, (err, data) => {
-            if (err) {
-              res.write(JSON.stringify({
-                code: 1,
-                msg: '数据库有误'
-              }));
-              res.end();
+//         } else if (data.length == 0) {
+//           res.write(JSON.stringify({
+//             code: 1,
+//             msg: '用户名不存在'
+//           }));
+//           res.end();
 
-            } else {
-              res.write(JSON.stringify({
-                code: 0,
-                msg: '登录成功'
-              }));
-              res.end();
-            }
-          });
-        }
-      });
+//         } else if (data[0].password != pass) {
+//           res.write(JSON.stringify({
+//             code: 1,
+//             msg: '用户名或密码错误'
+//           }));
+//           res.end();
 
-    }
+//         } else {
+//           //3.设置状态
+//           db.query(`UPDATE stu_user SET online=1 WHERE ID=${data[0].ID}`, (err, data) => {
+//             if (err) {
+//               res.write(JSON.stringify({
+//                 code: 1,
+//                 msg: '数据库有误'
+//               }));
+//               res.end();
 
-    console.log('请求了登录', query);
+//             } else {
+//               res.write(JSON.stringify({
+//                 code: 0,
+//                 msg: '登录成功'
+//               }));
+//               res.end();
+//             }
+//           });
+//         }
+//       });
 
-  } else {
-    //读文件
-    fs.readFile(`views${pathname}`, (err, data) => {
-      if (err) {
-        res.writeHead(404);
-        res.write('Not Found');
-      } else {
-        res.write(data);
-      }
+//     }
 
-      res.end();
-    });
-  }
-  
-});
+//     console.log('请求了登录', query);
 
-httpServer.listen(8080);
+//   } else {
+//     //读文件
+//     fs.readFile(`views${pathname}`, (err, data) => {
+//       if (err) {
+//         res.writeHead(404);
+//         res.write('Not Found');
+//       } else {
+//         res.write(data);
+//       }
+
+//       res.end();
+//     });
+//   }
+
+// });
 
